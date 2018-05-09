2d29cfa3e8f99ae7b9909012b907e78ffef640f3
Pasan Premaratne <pasan@premaratne.me>
2018-05-09 08:56:19 -0400

    S1V2

---
+++ Stormy/CurrentWeather.swift
@@ -0,0 +1,43 @@
+//
+//  CurrentWeather.swift
+//  Stormy
+//
+//  Created by Screencast on 5/9/18.
+//  Copyright © 2018 Treehouse. All rights reserved.
+//
+
+import Foundation
+
+struct CurrentWeather {
+    let temperature: Double
+    let humidity: Double
+    let precipProbability: Double
+    let summary: String
+    let icon: String
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
