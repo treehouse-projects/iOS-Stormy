98a4978ada11f2cbb4e19c0b5fe565f88aba68bb
Pasan Premaratne <pasan@premaratne.me>
2018-05-16 08:47:18 -0400

    S2V3

--- Stormy/ViewController.swift
+++ Stormy/ViewController.swift
@@ -17,9 +17,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var currentSummaryLabel: UILabel!
     @IBOutlet weak var refreshButton: UIButton!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    fileprivate let darkSkyApiKey = "b853321513ee430a225b12b5f02b8dd7"
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let base = URL(string: "https://api.darksky.net/forecast/\(darkSkyApiKey)/")
+        let forecastUrl = URL(string: "37.8267,-122.4233", relativeTo: base)
         
         let currentWeather = CurrentWeather(temperature: 85.0, humidity: 0.8, precipProbability: 0.1, summary: "Hot!", icon: "clear-day")
         let viewModel = CurrentWeatherViewModel(model: currentWeather)
