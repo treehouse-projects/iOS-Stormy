09bf9e7d1ef3e9cf7488216f1e842a0e862e4d42
Pasan Premaratne <pasan@premaratne.me>
2018-05-23 09:31:27 -0400

    S4V3

--- Stormy/DarkSkyAPIClient.swift
+++ Stormy/DarkSkyAPIClient.swift
@@ -17,9 +17,10 @@ class DarkSkyAPIClient {
     
     let downloader = JSONDownloader()
     
+    typealias WeatherCompletionHandler = (Weather?, DarkSkyError?) -> Void
     typealias CurrentWeatherCompletionHandler = (CurrentWeather?, DarkSkyError?) -> Void
     
-    func getCurrentWeather(at coordinate: Coordinate, completionHandler completion: @escaping CurrentWeatherCompletionHandler) {
+    private func getWeather(at coordinate: Coordinate, completionHandler completion: @escaping WeatherCompletionHandler) {
         
         guard let url = URL(string: coordinate.description, relativeTo: baseUrl) else {
             completion(nil, .invalidUrl)
@@ -29,7 +30,25 @@ class DarkSkyAPIClient {
         let request = URLRequest(url: url)
         
         let task = downloader.jsonTask(with: request) { json, error in
+            guard let json = json else {
+                completion(nil, error)
+                return
+            }
             
+            guard let weather = Weather(json: json) else {
+                completion(nil, .jsonParsingFailure)
+                return
+            }
+            
+            completion(weather, nil)
+        }
+        
+        task.resume()
+    }
+    
+    func getCurrentWeather(at coordinate: Coordinate, completionHandler completion: @escaping CurrentWeatherCompletionHandler) {
+        getWeather(at: coordinate) { weather, error in
+            completion(weather?.currently, error)
         }
     }
 }
